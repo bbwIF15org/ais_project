a1f9ef18af0a374b48cc93818617447ca4761b21
Johann <Johann@BBWS-IT-PC19>
2017-09-12 08:08:44 +0200

    Projektdateien hinzufügen.

---
+++ AIS_FI15/Controllers/PsychologischerDienstController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AIS_FI15.Controllers
+{
+    public class PsychologischerDienstController : Controller
+    {
+        // GET: PsychologischerDienst
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
