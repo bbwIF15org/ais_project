2743d8a812dd87f0aec49814bb8db85df7351563
Enrico <Enrico@test>
2017-09-13 14:27:59 +0200

    Login fertig gestellt mit XML-Datei. und pbkdf2 Cryptography.

--- AIS_FI15/Controllers/LoginController.cs
+++ AIS_FI15/Controllers/LoginController.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace AIS_FI15.Controllers
 {
@@ -18,24 +23,58 @@ namespace AIS_FI15.Controllers
         [HttpPost]
         public ActionResult Index(LoginModel model)
         {
-            if(ModelState.IsValid)
+
+            if (ModelState.IsValid)
             {
-                if(model.Username == "marten" && model.Password == "pw")
-                {
-                    FormsAuthentication.SetAuthCookie(model.Username, false);
-                    return RedirectToAction("index", "Verwaltung/Index");
-                }
+                XmlDocument doc = new XmlDocument();
+
+                doc.Load(Server.MapPath("/App_Data/Login.xml"));
+
+                foreach (XmlNode node in doc.SelectNodes("//user"))
 
                 {
-                    ModelState.AddModelError("", ""); // TODO besser beschreibung 
+                    String Username = node.SelectSingleNode("username").InnerText;
+                    String Password = node.SelectSingleNode("password").InnerText;
+
+                    //Crypto
+
+                    byte[] hashBytes = Convert.FromBase64String(Password);  //Extract
+
+                    byte[] salt = new byte[16];                             //Salt nehmen
+                    Array.Copy(hashBytes, 0, salt, 0, 16);
+
+                    var pbkdf2 = new Rfc2898DeriveBytes(model.Password, salt, 10000);
+                    byte[] hash = pbkdf2.GetBytes(20);
+
+                    /* Compare the results */
+
+                    for (int i = 0; i < 20; i++)
+                    {
+                        if (hashBytes[i + 16] != hash[i])
+                        { 
+                            ModelState.AddModelError("", ""); // TODO besser beschreibung 
+                            return View();
+                        }
+
+                    }
+
+
+                    if (model.Username == Username)
+                    {
+                        FormsAuthentication.SetAuthCookie(model.Username, false);
+                        return RedirectToAction("index", "Verwaltung/Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", ""); // TODO besser beschreibung 
+                    }
+  
                 }
-                
+
             }
-
 
             return View();
         }
-
 
     }
 }
--- AIS_FI15/Models/LoginModel.cs
+++ AIS_FI15/Models/LoginModel.cs
@@ -2,13 +2,19 @@
 using System.Linq;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace AIS_FI15.Controllers
 {
+    [XmlRoot("Benutzer")]
     public class LoginModel
     {
+        [XmlElement("Benutzername")]
         [Required(ErrorMessage = "Der Benutzername ist erforderlich.")]
         public string  Username { get; set; }
+
+        [XmlElement("Passwort-Hash")]
         [Required(ErrorMessage = "Das Passwort ist erforderlich.")]
         public string Password { get; set; }
     }
