7c7ee3374f1afdd8b007e183fe260f44707968e2
Enrico <Enrico@test>
2017-09-14 14:20:53 +0200

    Suchen per JavaScript und Controller.

---
+++ AIS_FI15/Controllers/SuchenController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AIS_FI15.Controllers
+{
+    public class SuchenController : Controller
+    {
+        // GET: Suchen
+        public ActionResult Index()
+        {
+           return null;
+           // return View();
+        }
+    }
+}
