2dee1157ce1cf7106cb9ed2126d26ca9c619b262
Johann <Johann@BBWS-IT-PC19>
2017-09-15 10:22:45 +0200

    Suchfunktion backend angefangen

--- AIS_FI15/Controllers/SuchenController.cs
+++ AIS_FI15/Controllers/SuchenController.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MySql.Data.MySqlClient;
 
 namespace AIS_FI15.Controllers
 {
     public class SuchenController : Controller
     {
         // GET: Suchen
-        public ActionResult Index()
+        public ActionResult Index(SuchenModel model)
         {
-           return null;
-           // return View();
+           return View();
         }
-    }
+
+        [HttpPost]
+        public ActionResult CheckTerm(SuchenModel model)
+        {
+            string Suchwort = model.Suchwort;
+
+            string conString = "server=localhost;uid=root;pwd=;database=ais_fi15;";
+            MySqlConnection con = new MySqlConnection(conString);
+
+            return null;
+        }
+
+
+
+        }
 }
---
+++ AIS_FI15/Models/SuchenModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AIS_FI15.Controllers
+{
+    public class SuchenModel
+    {
+        public string Suchwort { get; set; }
+    }
+}
