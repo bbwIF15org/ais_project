d7ef7393d347c34c561d7f5759333eaad6b7833f
Enrico <Enrico@test>
2017-11-06 14:40:11 +0100

    1

--- AIS_FI15/Controllers/SuchenController.cs
+++ AIS_FI15/Controllers/SuchenController.cs
@@ -4,16 +4,48 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MySql.Data.MySqlClient;
+using System.Web.Script.Serialization;
+using System.IO;
 
 namespace AIS_FI15.Controllers
 {
     public class SuchenController : Controller
     {
+
+   
+
+        //"[{Name:'Mensa',Link:'Mensa/Index'},{Name:'Kundenservice',Link:'Kundenservice/Index'}]";
+
+
+        public class Navigation
+        {
+            public string Link { get; set; }
+            public string Name { get; set; }
+        }
+
+
         // GET: Suchen
         public ActionResult Index(string Suchwort)
         {
 
-            Console.Write(Suchwort);
+
+            using (StreamReader r = new StreamReader(Server.MapPath("/App_Data/SearchStrings.json")))
+            {
+                string json = r.ReadToEnd();
+           
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                Navigation[] Navi = js.Deserialize<Navigation[]>(json);
+
+                for(int l = 0; l < Navi.Length; l++ )
+                {
+                       if(Navi[l].Name == Suchwort)
+                            return RedirectToAction("index", Navi[l].Link);
+                }
+
+
+
+            }
+
 
             return View();
         }
