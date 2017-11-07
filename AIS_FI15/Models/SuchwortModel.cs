bf4351443d3ee1c9009b7ce4232c34692fa8862c
Enrico <Enrico@test>
2017-11-07 11:19:32 +0100

    Suchen Model hinzugefügt

---
+++ AIS_FI15/Models/SuchwortModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace AIS_FI15.Models
+{
+    public class SuchwortModel
+    {
+      
+        public string SuchwortSend { get; set; }
+    }
+}
