931d9076f33b5e836a71840dabfa39a6c05dd918
Enrico <Enrico@test>
2017-11-07 08:01:01 +0100

    Suchfunktion bearbeitet

--- AIS_FI15/Models/SuchenModel.cs
+++ AIS_FI15/Models/SuchenModel.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
 
 namespace AIS_FI15.Controllers
 {
     public class SuchenModel
     {
         public string Suchwort { get; set; }
+
+        public class Navigation
+        {
+            public string site { get; set; }
+            public string word { get; set; }
+        }
+
+        public static string Auslesen(string p)
+        {
+                using (StreamReader r = new StreamReader(p))
+                {
+                    string json = r.ReadToEnd();
+
+                    return json;
+                }
+        }
+
+
     }
 }
