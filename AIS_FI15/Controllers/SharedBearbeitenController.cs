8ab79d4d41e077469ac423ea9d249ec5bf9dd71c
PC24 <PC24@BBW-IT-PC24>
2017-11-14 14:42:29 +0100

    Name der aufrufenden View an SharedAnzeigenController und SharedBearbeitenController weitergeben -> somit sind die "Shared*Controller" nun universell nutzbar

--- AIS_FI15/Controllers/SharedBearbeitenController.cs
+++ AIS_FI15/Controllers/SharedBearbeitenController.cs
@@ -14,92 +14,26 @@ namespace AIS_FI15.Controllers
     public class SharedBearbeitenController : Controller
     {
         // GET: SharedBearbeiten
-        public ActionResult Index(string U1, string U2, string TextArea1)
-        {
-            
+        public ActionResult Index(string U1, string U2, string TextArea1, string callingView)
+        {   
+            DbWrite(U1, U2, TextArea1, callingView);
 
-            
-            DbWrite(U1, U2, TextArea1, DateTime.Now);
-
-            return RedirectToAction("index", "Wohnheim/Index");
+            string controllerToRedirectTo = callingView + "/Index";
+            return RedirectToAction("index", controllerToRedirectTo);            
         }
-        public void DbWrite(string us, string uus, string txt, DateTime zp)//ToDo es muss noch die aufrufende Seite übergeben werden (z.B. "Wohnheim")
+        public void DbWrite(string us, string uus, string txt, string callingView)
         {
 
-            //var db = Database.Open("SQLServerConnectionString");
+            var db = Database.Open("SQLServerConnectionString");
 
             //var insertQuery = "INSERT INTO Wohnheim (Title, Addition, Text, Time) VALUES ('" + us + "','" + uus + "','" + txt + "', CURRENT_TIMESTAMP)";
 
-            string insertQuery = "INSERT INTO Wohnheim (Title, Addition, Text, Time) VALUES (@0, @1, @2, CURRENT_TIMESTAMP)";
+            string insertQuery = "INSERT INTO " + callingView + " (Title, Addition, Text, Time) VALUES (@0, @1, @2, CURRENT_TIMESTAMP)";
 
             db.Execute(insertQuery, new string[3] { us, uus, txt });
 
-            db.Execute(insertQuery);
-
-            db.Close();
+            db.Close();     
             
-
-            //Artikel a = new Artikel();
-            //a.ueberschrift = us;
-            //a.unterUeberschrift = uus;
-            //a.text = txt;
-            //a.zeitpunkt = zp;
-
-            //MemoryStream stream1 = new MemoryStream();
-            //DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Artikel));
-
-            //ser.WriteObject(stream1, a);
-            //stream1.Position = 0;
-            //StreamReader sr = new StreamReader(stream1);
-
-
-            //try
-            //{
-            //    FileStream fs = new FileStream(path, FileMode.Append);
-            //    StreamWriter sw = new StreamWriter(fs);
-            //    sw.WriteLine("," + sr.ReadToEnd());
-               
-            //    sw.Close();
-            //    fs.Close();
-            //}
-
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine(ex.ToString());
-            //}
         }
-     
-
-        //[DataContract]
-        //internal class Artikel
-        //{
-        //    [DataMember]
-        //    internal string ueberschrift;
-
-        //    [DataMember]
-        //    internal string unterUeberschrift;
-
-        //    [DataMember]
-        //    internal string text;
-
-        //    [DataMember]
-        //    internal DateTime zeitpunkt;
-        //}
-
-
     }
-}
-
-
-
-
-
-  // Open the stream and read it back.
-                    //using (StreamReader sr = System.IO.File.OpenText(path))
-                    //{
-                    //    string s = "";
-                    //    while ((s = sr.ReadLine()) != null)
-                    //    {
-                    //        Console.WriteLine(s);
-                    //    }
-                    //}
+} 
