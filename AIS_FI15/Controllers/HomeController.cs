3e809533ed4ad052907b1bc8adfddae2a3764ccc
PC24 <PC24@BBW-IT-PC24>
2017-11-16 07:56:04 +0100

    Speisepläne

--- AIS_FI15/Controllers/HomeController.cs
+++ AIS_FI15/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+using ppt = Microsoft.Office.Interop.PowerPoint;
+using System.Runtime.InteropServices;
 
 namespace AIS_FI15.Controllers
 {
