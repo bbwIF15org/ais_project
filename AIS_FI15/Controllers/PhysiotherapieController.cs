82fc452d1b1011c9b619857a51a551a2c2ece064
PC24 <PC24@BBW-IT-PC24>
2017-11-17 10:45:32 +0100

    Physiotherapie hinzugefügt

---
+++ AIS_FI15/Controllers/PhysiotherapieController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AIS_FI15.Controllers
+{
+    public class PhysiotherapieController : Controller
+    {
+        // GET: Physiotherapie
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
