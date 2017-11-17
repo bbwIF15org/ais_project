446ddb13306172bc4701dad0291f96febedb6d2a
Amedeo Molnar <nek0@nek0eu>
2017-11-17 13:27:32 +0100

    hinzufügen von titel zu öffnungszeit

--- AIS_FI15/Controllers/SharedBearbeitenController.cs
+++ AIS_FI15/Controllers/SharedBearbeitenController.cs
@@ -81,16 +81,16 @@ namespace AIS_FI15.Controllers
 
         //POST: Wohnheim
         [HttpPost]
-        public ActionResult InsertTime(string Name, string U1, string U2, string U3, string U4, string U5, string U6, string U7, string U8, string U9, string U10, string U11, string U12, string U13, string U14, string callingView)
+        public ActionResult InsertTime(string Name, string Titel, string U1, string U2, string U3, string U4, string U5, string U6, string U7, string U8, string U9, string U10, string U11, string U12, string U13, string U14, string callingView)
         {
 
             var db = Database.Open("SQLServerConnectionString");
 
             //var insertQuery = "INSERT INTO Wohnheim (Title, Addition, Text, Time) VALUES ('" + us + "','" + uus + "','" + txt + "', CURRENT_TIMESTAMP)";
 
-            string insertQuery = "INSERT INTO Zeiten (Name, MontagFr, MOntagSp, DienstagFr, DienstagSp, MittwochFr, MittwochSp, DOnnerstagFr, DonnerstagSp, FreitagFr, FreitagSp, SamstagFr, SamstagSp, SOnntagFr, SonntagSp) VALUES (@0, @1, @2, @3, @4, @5, @6, @7, @8, @9, @10, @11, @12, @13, @14)";
+            string insertQuery = "INSERT INTO Zeiten (Name, Titel, MontagFr, MOntagSp, DienstagFr, DienstagSp, MittwochFr, MittwochSp, DonnerstagFr, DonnerstagSp, FreitagFr, FreitagSp, SamstagFr, SamstagSp, SonntagFr, SonntagSp) VALUES (@0, @1, @2, @3, @4, @5, @6, @7, @8, @9, @10, @11, @12, @13, @14, @15)";
 
-            db.Execute(insertQuery, new string[15] { Name, U1, U2, U3, U4, U5, U6, U7, U8, U9, U10, U11, U12, U13, U14 });
+            db.Execute(insertQuery, new string[16] { Name, Titel, U1, U2, U3, U4, U5, U6, U7, U8, U9, U10, U11, U12, U13, U14 });
 
 
             db.Close();
