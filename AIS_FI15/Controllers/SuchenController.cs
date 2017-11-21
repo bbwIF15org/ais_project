70ccf05a46013cb400532b68dae858f54b59d9e1
anonym <anonym@hidden.private>
2017-11-21 10:48:12 +0100

    test

--- AIS_FI15/Controllers/SuchenController.cs
+++ AIS_FI15/Controllers/SuchenController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using MySql.Data.MySqlClient;
+//using MySql.Data.MySqlClient;
 using System.Web.Script.Serialization;
 using System.IO;
 
