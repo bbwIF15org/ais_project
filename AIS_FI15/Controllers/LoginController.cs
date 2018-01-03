0ac0aa3d32f691d67d9f370d201f6a662417a920
anonym <anonym@hidden.private>
2018-01-03 11:38:58 +0100

    Login fixed

--- AIS_FI15/Controllers/LoginController.cs
+++ AIS_FI15/Controllers/LoginController.cs
@@ -29,37 +29,17 @@ namespace AIS_FI15.Controllers
                 XmlDocument doc = new XmlDocument();
 
                 doc.Load(Server.MapPath("/App_Data/Login.xml"));
-
-                foreach (XmlNode node in doc.SelectNodes("//user"))
-
+                XmlNodeList UserList = doc.SelectSingleNode("Benutzer").ChildNodes;
+                //foreach (XmlNode node in doc.SelectNodes("user"))               
+                foreach (XmlNode User in UserList)
                 {
-                    String Username = node.SelectSingleNode("username").InnerText;
-                    String Password = node.SelectSingleNode("password").InnerText;
 
-                    //Crypto
+                    String Username = User.SelectSingleNode("username").InnerText;
+                    String Password = User.SelectSingleNode("password").InnerText;
 
-                    byte[] hashBytes = Convert.FromBase64String(Password);  //Extract
+                    bool pw_check = CheckPw(Password, model.Password);
 
-                    byte[] salt = new byte[16];                             //Salt nehmen
-                    Array.Copy(hashBytes, 0, salt, 0, 16);
-
-                    var pbkdf2 = new Rfc2898DeriveBytes(model.Password, salt, 10000);
-                    byte[] hash = pbkdf2.GetBytes(20);
-
-                    /* Compare the results */
-
-                    for (int i = 0; i < 20; i++)
-                    {
-                        if (hashBytes[i + 16] != hash[i])
-                        {
-                            ModelState.AddModelError("", ""); // TODO besser beschreibung 
-                            return View();
-                        }
-
-                    }
-
-
-                    if (model.Username == Username)
+                    if (model.Username == Username && pw_check)//Nutzername UND Passwort müssen übereinstimmen
                     {
                         FormsAuthentication.SetAuthCookie(model.Username, false);
                         return RedirectToAction("index", "Verwaltung/Index");
@@ -74,6 +54,38 @@ namespace AIS_FI15.Controllers
             }
 
             return View();
+        }
+        /// <summary>
+        /// Vergleicht das eingegebene mit dem Gespeicherten Passwort
+        /// </summary>
+        /// <param name="PW">gespeichertes Passwort</param>
+        /// <param name="mPW">eingegebenes Passwort</param>
+        /// <returns>true, wenn Paswort übereinstimmt, false wenn es nicht übereinstimmt</returns>
+        private bool CheckPw(string PW, string mPW)
+        {
+            //Crypto
+
+            byte[] hashBytes = Convert.FromBase64String(PW);  //Extract
+
+            byte[] salt = new byte[16];                             //Salt nehmen
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(mPW, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            /* Compare the results */
+
+            for (int i = 0; i < 20; i++)
+            {
+                if (hashBytes[i + 16] != hash[i])
+                {
+                    //Passwörter stimmen nicht überein
+                    //Abbruch schon hier
+                    return false;
+                }
+
+            }
+            return true;
         }
 
     }
